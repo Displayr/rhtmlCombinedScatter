869ecc018ba2c6f420d3849c453047520ae468bf
chschan <chschan@users.noreply.github.com>
2024-02-26 16:43:10 +1100

    Skip makeDocs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 const gulp = require('gulp')
 const rhtmlBuildUtils = require('rhtmlBuildUtils')
 
-const dontRegisterTheseTasks = []
+// Temporarily skipping makeDocs because circle CI does not install R
+gulp.task('makeDocs', gulp.series(function (done) {
+  console.log('skipping makeDocs')
+  done()
+}))
+
+const dontRegisterTheseTasks = [ 'makeDocs' ]
 rhtmlBuildUtils.registerGulpTasks({ gulp, exclusions: dontRegisterTheseTasks })
