f2e45fac565c2e993038e59f57bf71acb53f6ac0
chschan <chschan@users.noreply.github.com>
2024-02-29 12:19:01 +1100

    Use new release of rhtmlBuildUtils

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,4 @@
 const gulp = require('gulp')
 const rhtmlBuildUtils = require('rhtmlBuildUtils')
-
-// Temporarily skipping makeDocs because circle CI does not install R
-gulp.task('makeDocs', gulp.series(function (done) {
-  console.log('skipping makeDocs')
-  done()
-}))
-
-const dontRegisterTheseTasks = [ 'makeDocs' ]
+const dontRegisterTheseTasks = []
 rhtmlBuildUtils.registerGulpTasks({ gulp, exclusions: dontRegisterTheseTasks })
