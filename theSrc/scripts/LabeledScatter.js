695d89587d6b06cdbcb2a6c5ffeeb862686fd088
Justin Yap <JustinCCYap@users.noreply.github.com>
2024-03-13 16:05:29 +1100

    Fix for resize test

--- theSrc/scripts/LabeledScatter.js
+++ theSrc/scripts/LabeledScatter.js
@@ -161,7 +161,6 @@ class LabeledScatter {
           console.log('rhtmlLabeledScatter: resize timeout')
 
           const resizeParams = this.resizeStack.pop()
-          const el = resizeParams[0]
           const width = resizeParams[1]
           const height = resizeParams[2]
           this.resizeStack = []
@@ -169,29 +168,7 @@ class LabeledScatter {
           this.width = width
           this.height = height
 
-          if (typeof this.plot === 'undefined') {
-            this.draw()
-          } else {
-            d3.select(el).select('.plot-container').remove()
-            const svg = d3.select(el)
-                    .append('svg')
-                    .attr('width', this.width)
-                    .attr('height', this.height)
-                    .attr('class', 'plot-container rhtmlwidget-outer-svg')
-
-            this.plot.resized(svg, this.width, this.height)
-              .catch(err => {
-                if (
-                  err.type === InsufficientHeightError.type ||
-                  err.type === InsufficientWidthError.type
-                ) {
-                  console.log(`caught expected error '${err.type}' and aborted rendering`)
-                  DisplayError.displayEmptyErrorContainer(this.rootElement)
-                } else {
-                  throw err
-                }
-              })
-          }
+          this.draw()
 
           // TODO this should be in a then/catch/finally attached to this.plot.resized but not going to attempt that now
           this.resizeDelayPromise = null
