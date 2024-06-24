20c52a936726e4095a05a2b0b205f02865379fd5
Justin Yap <JustinCCYap@users.noreply.github.com>
2024-06-24 16:17:01 +1000

    FS2-2574: hide shared axes (#44)
    * Hide shared axes
    
    * Fix
    
    * Comment
    
    * Hide ticks
    
    * Spacing
    
    * Accept snapshots

--- theSrc/scripts/PlotlyChartElements.js
+++ theSrc/scripts/PlotlyChartElements.js
@@ -443,8 +443,6 @@ function createPlotlyLayout (config, margin_right, height) {
 
     const plot_layout = {
         grid: grid,
-        xaxis: x_axis,
-        yaxis: y_axis,
         title: {
             text: config.title,
             font: {
@@ -476,12 +474,7 @@ function createPlotlyLayout (config, margin_right, height) {
         plot_bgcolor: config.plotAreaBackgroundColor,
         height: chartHeight(config, height)
     }
-    if (npanel >= 2) {
-        for (let p = 2; p <= npanel; p++) {
-            plot_layout['xaxis' + p] = x_axis
-            plot_layout['yaxis' + p] = y_axis
-        }
-    }
+    addAxesToGrid(plot_layout, x_axis, y_axis, npanel, config.panelNumRows, config.panelShareAxes)
     if (config.subtitle.length > 0) {
         plot_layout.annotations = [{
             name: 'subtitle',
@@ -922,6 +915,39 @@ function wrapByNumberOfCharacters (text, n_char) {
         lines.push(`${current_line.join(' ')}`)
     }
     return lines.join('<br>')
+}
+
+function addAxesToGrid (plot_layout, x_axis, y_axis, n_panels, n_rows, share_axes) {
+    n_rows = Math.min(n_rows, n_panels)
+    if (share_axes && n_rows > 1) {
+        // Hide x-axis of first panel if there is more than one row
+        plot_layout.xaxis = hideAxis(_.clone(x_axis))
+    } else {
+        plot_layout.xaxis = x_axis
+    }
+    plot_layout.yaxis = y_axis
+
+    const n_cols = Math.ceil(n_panels / n_rows)
+    for (let p = 2; p <= n_panels; p++) {
+        if (share_axes && p <= n_panels - n_cols) {
+            // Hide x-axis of subsequent panels if they aren't the bottommost
+            plot_layout['xaxis' + p] = hideAxis(_.clone(x_axis))
+        } else {
+            plot_layout['xaxis' + p] = x_axis
+        }
+        if (share_axes && n_cols > 1 && p % n_cols !== 1) {
+            // Hide y-axis of subsequent panels if they aren't the leftmost
+            plot_layout['yaxis' + p] = hideAxis(_.clone(y_axis))
+        } else {
+            plot_layout['yaxis' + p] = y_axis
+        }
+    }
+}
+
+function hideAxis (axis) {
+    axis.showticklabels = false
+    axis.ticklen = 0
+    return axis
 }
 
 module.exports = {
