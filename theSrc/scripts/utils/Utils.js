df578a4432184486682fb8fb6f372c4b844f69d4
Justin Yap <JustinCCYap@users.noreply.github.com>
2024-07-03 15:41:51 +1000

    Add opacity to color scale

--- theSrc/scripts/utils/Utils.js
+++ theSrc/scripts/utils/Utils.js
@@ -85,6 +85,24 @@ class Utils {
     span.remove()
     return size
   }
+
+  static addOpacity (color, opacity) {
+    if (this.isRgb(color) && opacity !== null) {
+      color = `${color.substring(0, color.length - 1)},${opacity})`
+    }
+    if (this.isHexColorWithoutOpacity(color) && opacity !== null) {
+      color = `${color}${Math.floor(opacity * 255).toString(16)}`
+    }
+    return color
+  }
+
+  static isRgb (color) {
+    return color.match(/^rgb(\d+,\d+,\d+)$/)
+  }
+
+  static isHexColorWithoutOpacity (color) {
+    return color.match(/#[\d,A-F,a-f]{6}/)
+  }
 }
 
 module.exports = Utils
